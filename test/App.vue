f39c8d08558363eadf885d2e8e617e0fe7be6c82
yuzhaoxing <yu.zhaoxing110@iwhalecloud.com>
2020-07-02 16:39:49 +0800

    :roctet: update demo

---
+++ test/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <mc-button>按钮</mc-button>
+  </div>
+</template>
+
+<script>
+import McButton from "./TestButton";
+export default {
+  name: "app",
+  components: {
+    McButton
+  }
+};
+</script>
+
+<style lang="scss"></style>
